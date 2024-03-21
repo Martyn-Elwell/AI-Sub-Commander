194c53b3a0382e181e5f48449cc7f24ec54f5b67
Martyn <martyn.elwell@btopenworld.com>
2024-03-21 18:12:31 +0000

    outline shader failed

--- Assets/Shaders/PostProcessOutline.cs
+++ Assets/Shaders/PostProcessOutline.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
-public class PostProcessOutline : MonoBehaviour
+// Shader Settings
+[Serializable]
+[PostProcess(typeof(PostProcessOutlineRenderer), PostProcessEvent.AfterStack, "Outline")]
+public sealed class PostProcessOutline : PostProcessEffectSettings
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public FloatParameter thickness = new FloatParameter { value = 1f };
+    public FloatParameter depthMin = new FloatParameter { value = 0f };
+    public FloatParameter depthMax = new FloatParameter { value = 1f };
+}
 
-    // Update is called once per frame
-    void Update()
+public class PostProcessOutlineRenderer : PostProcessEffectRenderer<PostProcessOutline>
+{
+    public override void Render(PostProcessRenderContext context)
     {
-        
+        PropertySheet sheet = context.propertySheets.Get(Shader.Find("Hidden/Outline"));
+        sheet.properties.SetFloat("_Thickness", settings.thickness);
+        sheet.properties.SetFloat("_DepthMin", settings.depthMin);
+        sheet.properties.SetFloat("_DepthMax", settings.depthMax);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
+
