40455e2f81fc1a0494959b9cfef83f96e2c298de
Martyn <martyn.elwell@btopenworld.com>
2024-03-28 18:33:37 +0000

    not a lot of work tbh

---
+++ Assets/Scripts/Interfaces/Room.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> breaches;
+    [SerializeField] private List<GameObject> covers;
+    [SerializeField] private List<GameObject> sabotages;
+    [SerializeField] private List<GameObject> enemies;
+}
